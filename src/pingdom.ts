894acedd7c5f2d21360317bf49d41ca3b7a2a5c8
ofirdagan <ofirdagan@gmail.com>
2021-11-04 01:47:41 +0200

    feat(security): add apiKey

--- src/pingdom.ts
+++ src/pingdom.ts
@@ -2,11 +2,11 @@ import axios from 'axios';
 
 const THREE_MIN = 1000 * 60 * 3;
 
-export function monitor(serviceName: string) {
-  reportAlive(serviceName);
-  setInterval(() => reportAlive(serviceName), THREE_MIN);
+export function monitor(serviceName: string, apiKey: string) {
+  reportAlive(serviceName, apiKey);
+  setInterval(() => reportAlive(serviceName, apiKey), THREE_MIN);
 }
 
-function reportAlive(serviceName: string) {
-  axios.get(`https://ofird11.editorx.io/pingdom/_functions/ping/${serviceName}`);
+function reportAlive(serviceName: string, apiKey: string) {
+  axios.get(`https://ofird11.editorx.io/pingdom/_functions/ping/${serviceName}`, {headers: {'x-api-key': apiKey}});
 }
